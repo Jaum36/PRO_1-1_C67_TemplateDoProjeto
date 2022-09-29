eba0ec9f4678602bdd60c9cd89ee51e8e5cf9d2b
João <joaovictormattoslopes@gmail.com>
2022-09-29 19:49:06 -0400

    Adicionei o banco de dados

--- config.js
+++ config.js
@@ -2,6 +2,12 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyD30Qkw-XjJaDDPZry1TPLw1qcZ3-3bpz8",
+    authDomain: "projeto67-d3562.firebaseapp.com",
+    projectId: "projeto67-d3562",
+    storageBucket: "projeto67-d3562.appspot.com",
+    messagingSenderId: "773821105248",
+    appId: "1:773821105248:web:411cf68ca235fd83f56f05"
 
 };
 // Initialice o Firebase
